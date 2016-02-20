f8cad5a36a147cad1503db0fa65593d4ec3a7610
jvalen <javiervalenciaromero@gmail.com>
2016-02-20 03:48:14 -0500

    gorilla refactor from class to factory

---
+++ src/js/body.js
@@ -0,0 +1,11 @@
+let body = {
+  bodyType: 'body',
+  screen: null,
+  size: null,
+  center: null,
+
+  update() {},
+  draw() {}
+}
+
+export default body;
--- src/js/gorilla.js
+++ src/js/gorilla.js
@@ -1,21 +1,16 @@
 import {drawRect} from './utils.js'
+import body from './body.js'
 
-class Gorilla {
-  constructor(screen, size, pos) {
-    this.screen = screen;
-    this.size = size;   
-    this.center = pos;
-    
-    console.log(pos);
-  }
-  
-  update() {
-    
-  }
-  
-  draw() {
-    drawRect(this.screen, this, 'black');
-  }
+let gorilla = function gorillaFactory(screen, size, pos) {
+  return Object.assign(Object.create(body), {
+    screen: screen,
+    size: size,
+    center: pos,
+    update() {},
+    draw() {
+      drawRect(this.screen, this, 'black');
+    }
+  });
 }
-  
-export default Gorilla;
+
+export default gorilla;
