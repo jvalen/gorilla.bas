86b60485cd070767e4daa63bb7d027bcfee15169
jvalen <javiervalenciaromero@gmail.com>
2016-02-21 23:10:27 -0500

    banana refactor from class to factory

--- src/js/banana.js
+++ src/js/banana.js
@@ -1,33 +1,31 @@
 import {drawRect} from './utils.js'
+import body from './body.js'
 
-class Banana {
-  constructor(screen, size, pos, vx, vy, gravity) {
-    this.screen = screen;
-    this.size = size;   
-    this.center = pos;
-    this.vx = vx;
-    this.vy = vy;
-    this.gravity = gravity;
-  }
-  
-  update() {
-    this.center.x += this.vx;
-    this.center.y += this.vy;
-    this.vy += this.gravity;
-  }
-  
-  draw() {
-    drawRect(this.screen, this, 'yellow');
-  }
-  
-  setPosition(pos) {
-    this.center = pos;
-    //console.log(pos);
-  }
-  
-  setTrajectoryValues(...args) {
-    [this.vx, this.vy, this.gravity] = args;
-  }
+let banana = function bananaFactory(screen, size, pos, vx, vy, gravity) {
+  return Object.assign(Object.create(body), {
+    type: 'banana',
+    screen: screen,
+    size: size,
+    center: pos,
+    vx: vx,
+    vy: vy,
+    gravity: gravity,
+    update() {
+      this.center.x += this.vx;
+      this.center.y += this.vy;
+      this.vy += this.gravity;
+    },
+    draw() {
+      drawRect(this.screen, this, 'yellow');
+    },
+    setPosition(pos) {
+      this.center = pos;
+      //console.log(pos);
+    },
+    setTrajectoryValues(...args) {
+      [this.vx, this.vy, this.gravity] = args;
+    }
+  });
 }
-  
-export default Banana;
+
+export default banana;
--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,20 @@
-import Gorilla from './gorilla.js';
-import Banana from './banana.js';
+import gorilla from './gorilla.js';
+import banana from './banana.js';
 import {addClass, removeClass, colliding, randomIntInRange} from './utils.js'
 
 ;(function(){
   let Game = function() {
     const gorillaSize = { width: 20, height: 20 };
     const bananaSize = { width: 15, height: 5 };
-    
+
     let screen = document.getElementById("screen").getContext('2d'),
         size = {
-          width: screen.canvas.width, 
+          width: screen.canvas.width,
           height: screen.canvas.height
         },
         center = { x: size.width / 2, y: size.height / 2 },
         self = this;
-    
+
     this.gameState = {
       state: 'player',
       nextPlayer: 'p1',
@@ -25,36 +25,36 @@ import {addClass, removeClass, colliding, randomIntInRange} from './utils.js'
       maxVitories: 3,
       impact: false
     };
-    
+
     this.bodies = [
-      new Gorilla(
-        screen, 
-        gorillaSize, 
-        { 
-          x: randomIntInRange(50, size.width / 6), 
-          y: randomIntInRange(50, size.height / 2) 
-        }
-      ),
-      new Gorilla(
-        screen, 
+      gorilla(
+        screen,
         gorillaSize,
-        { 
-          x: randomIntInRange(size.width / 2, size.width - 50), 
-          y: randomIntInRange(50, size.height / 2) 
+        {
+          x: randomIntInRange(50, size.width / 6),
+          y: randomIntInRange(50, size.height / 2)
         }
       ),
-      new Banana(
-        screen, 
+      gorilla(
+        screen,
+        gorillaSize,
+        {
+          x: randomIntInRange(size.width / 2, size.width - 50),
+          y: randomIntInRange(50, size.height / 2)
+        }
+      ),
+      banana(
+        screen,
         bananaSize,
         { x: 0, y: 0 },
         0, 0, 0
       )
     ];
-    
+
     this.nextState('player', {
       nextPlayer: 'p1'
     });
-    
+
     let tick = function() {
       self.update(size);
       self.draw(screen, size);
@@ -63,7 +63,7 @@ import {addClass, removeClass, colliding, randomIntInRange} from './utils.js'
 
     tick();
   }
-  
+
   Game.prototype = {
     update: function(size) {
       switch (this.gameState.state) {
@@ -95,22 +95,22 @@ import {addClass, removeClass, colliding, randomIntInRange} from './utils.js'
               },
               outOfBounds = function(b){
                 return (
-                  b.center.x > size.width || 
+                  b.center.x > size.width ||
                   b.center.x < 0 ||
-                  b.center.y > size.height || 
+                  b.center.y > size.height ||
                   b.center.y < 0
-                ) 
+                )
               };
-          
+
           for (var i = 0; i < this.bodies.length; i++) {
             if (this.bodies[i].update !== undefined) {
               this.bodies[i].update();
             }
-            
-            if (this.bodies[i] instanceof Banana) {
+
+            if (this.bodies[i] instanceof banana) {
               let collide = collidingWithSomething(this.bodies[i]),
-                  out = outOfBounds(this.bodies[i]); 
-              
+                  out = outOfBounds(this.bodies[i]);
+
               if (collide || out) {
                 this.nextState('player', {
                   nextPlayer: this.gameState.nextPlayer === 'p1' ? 'p2' : 'p1',
@@ -118,10 +118,10 @@ import {addClass, removeClass, colliding, randomIntInRange} from './utils.js'
                 });
               }
             }
-            
+
           }
           break;
-          
+
         case 'game-over':
           break;
       };
@@ -132,77 +132,77 @@ import {addClass, removeClass, colliding, randomIntInRange} from './utils.js'
       for (var i = 0; i < this.bodies.length; i++) {
         this.bodies[i].draw(screen);
       }
-      
+
     },
 
     nextState: function(newState, data) {
       switch (newState) {
         case 'select-names':
           break;
-          
+
         case 'player':
           let otherPlayer = data.nextPlayer === 'p1' ? 'p2' : 'p1',
               pWrapper = document.getElementById('values-' + data.nextPlayer),
               pWrapperOther = document.getElementById('values-' + otherPlayer),
               pAngle = document.getElementById('angle-' + otherPlayer),
               pVelocity = document.getElementById('velocity-' + otherPlayer);
-          
+
           //Reset last player's fields
           pAngle.value = '';
           pVelocity.value = '';
           pWrapperOther.setAttribute('data-ready', false);
           addClass(pWrapperOther, 'hide');
-    
+
           //Show current player fields
           removeClass(pWrapper, 'hide');
-          
+
           if (data.collide) {
             this.gameState.victories[this.gameState.nextPlayer]++;
           }
-          
-          let positionCurrentPlayer = 
-              data.nextPlayer === 'p1' ? 
+
+          let positionCurrentPlayer =
+              data.nextPlayer === 'p1' ?
                 this.bodies[0].center : this.bodies[1].center;
-          
+
           //Banana
           this.bodies[2].setPosition(
             { x: positionCurrentPlayer.x, y: positionCurrentPlayer.y - 20 }
           );
-          
+
           this.gameState.nextPlayer = data.nextPlayer;
-          
+
           console.log(
-            'p1: ' + this.gameState.victories.p1 + 
-            '\n' + 
+            'p1: ' + this.gameState.victories.p1 +
+            '\n' +
             'p2: ' + this.gameState.victories.p2 +
             '\n player: ' + this.gameState.nextPlayer + ' moves'
           );
-          
+
           break;
-          
+
         case 'projectile':
-          let banana = this.bodies.filter((x) => x instanceof Banana),
+          let currentBanana = this.bodies.filter((x) => (x.type === 'banana')),
               vx = parseInt(data.velocity, 10),
               angle = data.angle,
               vy = -(vx * Math.sin(angle * Math.PI / 180)),
               gravity = 0.1;
-                        
+
           if (this.gameState.nextPlayer === 'p2') {
             vx *= -1;
           }
-          
-          banana[0].setTrajectoryValues(vx, vy, gravity);
+
+          currentBanana[0].setTrajectoryValues(vx, vy, gravity);
           break;
-          
+
         case 'game-over':
           break;
       };
-      
+
       this.gameState.state = newState;
     }
   };
-  
-  
+
+
   window.addEventListener('load', function load(event) {
     window.removeEventListener("load", load, false);
     new Game();
